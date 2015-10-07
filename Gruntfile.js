3c7fa7d390a1042237026641d8c5591629ce0545
chedoddington <che.doddington@gmail.com>
2015-10-07 14:17:19 -0700

    fixed typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
 
   // Automatically load required grunt tasks
   require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin'
+    useminPrepare: 'grunt-usemin',
     buildcontrol: 'grunt-build-control'
   });
 
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:chedoddington/New-Dev-Env.git',
           branch: 'gh-pages'
         }
       }
